be7e5e85fefd8699b7ef0830a9a90f72ab414551
WoIfey <wuflerz@outlook.com>
2023-03-10 13:33:51 +0200

    throw-dice

---
+++ throw-dice/script.js
@@ -0,0 +1,14 @@
+document.getElementById("throw").addEventListener("click", function () {
+  let roll = Math.floor(Math.random() * 6) + 1;
+  let dice = document.getElementById("dice");
+
+  setTimeout(() => {
+    document.getElementById("number").innerText = roll;
+    dice.setAttribute("src", `/throw-dice/dices/${roll}.png`);
+  }, 1000);
+
+  dice.classList = "throw";
+  setTimeout(() => {
+    dice.classList = "";
+  }, 1000);
+});
