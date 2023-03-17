7c64c409c09fb06fa992a3c829d6cce2bc19d008
Philip Huynh <87246869+WoIfey@users.noreply.github.com>
2023-03-17 14:33:14 +0200

    Tasks

--- countdownFrom10/script.js
+++ countdownFrom10/script.js
@@ -1,15 +1,15 @@
 let secondsLeft = 10;
-const countdown = document.getElementById("countdown")
+const countdown = document.getElementById("countdown");
 const tick = setInterval(function () {
   if (secondsLeft == 0) {
     clearInterval(tick);
-    countdown.innerHTML = "Hej på dig!";
+    countdown.innerHTML = "Hello to you!";
     countdown.style.color = "white";
     document.querySelector("body").style.background = "red";
   } else if (secondsLeft == 1) {
-    countdown.innerHTML = secondsLeft + " sekund";
+    countdown.innerHTML = secondsLeft + " second";
   } else {
-    countdown.innerHTML = secondsLeft + " sekunder";
+    countdown.innerHTML = secondsLeft + " seconds";
   }
   secondsLeft -= 1;
 }, 1000);
