6a028892ad47419a6d531a41a3fd5c5c304b02c3
WoIfey <wuflerz@outlook.com>
2023-08-31 13:40:32 +0300

    Updates

--- serverProject/server/index.js
+++ serverProject/server/index.js
@@ -3,23 +3,23 @@ const path = require("path")
 const server = express()
 let fs = require("fs")
 
-server.use(express.urlencoded({ extended: true }))
-server.use(express.static(path.resolve("../client")))
+server.use(express.urlencoded())
+server.use(express.static(path.resolve("client")))
 
 server.post('/signup', (req, res) => {
     let users = []
-    const data = fs.readFileSync('info.json', 'utf8')
+    const data = fs.readFileSync('server/info.json', 'utf8')
 
     users = JSON.parse(data)
     users.push(req.body)
 
-    fs.writeFileSync('info.json', JSON.stringify(users))
+    fs.writeFileSync('server/info.json', JSON.stringify(users))
     res.redirect("/login")
 })
 
 server.post('/login', (req, res) => {
     const user = req.body
-    const file = JSON.parse(fs.readFileSync('info.json', 'utf8'))
+    const file = JSON.parse(fs.readFileSync('server/info.json', 'utf8'))
 
     const find = file.find((users) => users.user === user.user && users.password === user.password)
 
@@ -31,15 +31,15 @@ server.post('/login', (req, res) => {
 })
 
 server.get('/login', (req, res) => {
-    res.sendFile(path.resolve('../client/login.html'))
+    res.sendFile(path.resolve('client/login.html'))
 })
 
 server.get('/signup', (req, res) => {
-    res.sendFile(path.resolve('../client/signup.html'))
+    res.sendFile(path.resolve('client/signup.html'))
 })
 
 server.get('/home', (req, res) => {
-    res.sendFile(path.resolve('../client/home.html'))
+    res.sendFile(path.resolve('client/home.html'))
 })
 
 server.listen(3000, () => {
